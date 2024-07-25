b72b0b314fca0409e65af7353d8449ddd2ff14fd
hikihun <skh140@naver.com>
2024-07-25 23:15:09 +0900

    test: 의존관계 주입 방법 테스트
     - 의존관계 주입은 크게 4가지 방법이 있다.
       - 생성자 주입
         - 생성자를 통해 의존관계를 주입 받는 방법
         - 생성자 호출 시점에 1번만 호출 되는 것이 보장된다.
         - 그래서 불변, 필수 의존관계에서 서용한다.
       - 수정자 주입(setter 주입)
         - 자바빈 프로퍼티 규약의 수정자 메서드 방식을 사용하는 방법
         - 선택, 변경 가능성이 있는 의존관계에 사용한다.
       - 필드 주입
         - 말그대로 필드에 바로 주입하는 방법
         - 외부에서 변경이 불가능해서 테스트하기 힘들다는 치명적인 단점이 있다.
         - 사용을 지양할 것!
       - 일반 메서드 주입
         - 생성자 주입과 거의 유사하나, 일반적으로 잘 사용하지 않는다.
         - 한번에 여러 필드를 주입 받을 수 있다.

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -14,8 +14,9 @@ import org.springframework.context.annotation.FilterType;
 //ComponentScan을 했을 때 AppConfig도 스캔되기 때문에 임시로 @Configuration 어노테이션이 있는 컴포넌트를 제외함
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+    //수동 빈 등록과 자동 빈 등록의 충돌로 스프링부트 테스트시에 에러가 발생해서 주석처리 함
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,10 +9,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
+//    // 일반 메서드 주입 -> 생성자 주입과 거의 유사하다. 일반적으로 잘 사용하지 않는다.
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+//    //필드 주입 -> 필드에 바로 주입하는 방법 (사용을 지양할 것!, 테스트 코드에서의 사용은 괜찮)
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
+
+//    //수정자 주입 -> 선택, 변경 가능성이 있는 의존관계에 사용
+//    @Autowired(required = false)
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    //수정자 주입
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+//    @Autowired -> 생성자 1개 일때 생략 해도 자동 주입됨  //생성자 주입 -> 불변, 필수 의존관계에 사용
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
