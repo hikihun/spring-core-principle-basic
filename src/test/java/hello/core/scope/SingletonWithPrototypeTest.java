8b432e345190b1c5c025529944508897568f20c2
hikihun <skh140@naver.com>
2024-07-26 22:16:08 +0900

    test: 싱글톤 빈과 프로토타입 빈을 함께 사용시 문제점 해결방법 1
     - 프로토타입 빈이 싱글톤 빈과 함께 유지 되는 문제를 해결하는 가장 간단한 방법은 싱글톤 빈이 프로토타입을 사용할 때 마다 스프링 컨테이너에 새로 요청하는 것이다.
     - 테스트 결과 getBean()을 통해서 항상 새로운 프로토타입 빈이 생성되는 것을 확인 할 수 있다.
     - 이렇게 의존관계를 외부에서 주입(DI) 받는게 아니라 직접 필요한 의존관계를 찾는 것을 Dependency Lookup(DL) 의존관계 조회(탐색) 이라한다.
     - 하지만 이 코드 처럼 스프링 애플리케이션 컨텍스트 전체를 주입받게 되면, 스프링 컨테이너에 종속적인 코드가 되고, 단위 테스트도 어려워진다.
     - 그래서 필요한 기능이 지정한 프로토타입 빈을 컨테이너에서 대신 찾아주는 DL 기능만 있으면 좋을 것이다.

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -35,20 +35,20 @@ public class SingletonWithPrototypeTest {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-
-        private final PrototypeBean prototypeBean;  //생성시점에 주입됨
+        private final ApplicationContext applicationContext;
 
         @Autowired
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        ClientBean(ApplicationContext applicationContext) {
+            this.applicationContext = applicationContext;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
