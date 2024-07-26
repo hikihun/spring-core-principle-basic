2553c2e4ea93efb15e596ec95d4fe658ae87db86
hikihun <skh140@naver.com>
2024-07-26 15:31:23 +0900

    test: 조회 빈이 2개 이상일 때 해결 방법
     - @Autowired 필드명, 마라미터 이름으로 매칭
     - @Qualifier를 사용하여 추가 구분자를 붙여주는 방법
     - @Primary를 사용하여 우선순위를 정하는 방법

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -3,7 +3,9 @@ package hello.core.scan;
 import static org.assertj.core.api.Assertions.*;
 
 import hello.core.AutoAppConfig;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,10 +13,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AutoAppConfigTest {
 
     @Test
-    void basicTest() {
+    void basicScan() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
